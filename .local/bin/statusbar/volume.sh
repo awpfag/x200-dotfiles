c58da4a634f9e603af45ee975e7b2fe0b5543cab
luckyy777 <olli3tayl0r2003@gmail.com>
2023-04-21 23:49:45 +0100

    random little changes

--- .local/bin/statusbar/volume.sh
+++ .local/bin/statusbar/volume.sh
@@ -3,8 +3,10 @@ volume="$(pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+(?=%)' | head -n 1
 muted="$(pactl list sinks | grep 'Mute:' | tail -1 | tr -d '[:space:]')"
 icon=" "
 
-if [ "$muted" = "Mute: yes" ]; then
+if [ "$muted" = "Mute:yes" ]; then
 	volume="xx"
 fi
+
+echo "$muted"
 
 echo "$icon$volume"
